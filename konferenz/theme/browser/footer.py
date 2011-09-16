f8b95cb93e2f62d50adc1a86d4151d5bf728412e
Stefan Antonelli <stefan.antonelli@operun.de>
2011-09-16 11:49:39 +0000

    - updated theme to fit flyer - do not use special fonts anymore - simplified layout
    git-svn-id: http://svn.operun.de/svn/konferenz/konferenz.theme/trunk@3 e724e7e9-2db1-4b50-8bfa-12963a624e1d

--- konferenz/theme/browser/footer.py
+++ konferenz/theme/browser/footer.py
@@ -7,10 +7,25 @@ from plone.app.layout.viewlets.common import ViewletBase
 from plone.memoize.instance import memoize
 
 from datetime import date
+from DateTime import DateTime
+
 from random import shuffle
 
 class FooterViewlet(ViewletBase):
     index = ViewPageTemplateFile('templates/footer.pt')
     
     def year(self):
-        return date.today().year    
+        return date.today().year
+    
+    def greeting(self):
+        """return greeting based on time"""
+        
+        hour = DateTime().strftime('%H')
+        if hour in ['03','04','05','06','07','08','09','10','11']:
+            return 'Sie sehen gut aus heute Morgen'
+        
+        if hour in ['12','13','14','15','16','17']:
+            return 'Sie sehen gut aus heute'
+        
+        if hour in ['18','19','20','21','22','23','00','01','02']:
+            return 'Sie sehen gut aus heute Abend'
