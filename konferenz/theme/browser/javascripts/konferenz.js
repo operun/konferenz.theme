8a4fef2d2656d5fab6c8fc4eccba13fd79b83566
Stefan Antonelli <stefan.antonelli@operun.de>
2011-09-28 10:11:14 +0000

    - add some javascript to improove usability of the registration formula
    git-svn-id: http://svn.operun.de/svn/konferenz/konferenz.theme/trunk@13 e724e7e9-2db1-4b50-8bfa-12963a624e1d

---
+++ konferenz/theme/browser/javascripts/konferenz.js
@@ -0,0 +1,20 @@
+jq(document).ready(function() {
+
+	// helper script for the registration formula
+
+	jq("#fruehbucher-eintritt").click(function() {
+		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
+	});
+
+	jq("#regulaerer-eintritt").click(function() {
+		jq("#fruehbucher-eintritt").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
+	});
+
+	jq("#student-hochschule-muenchen").click(function() {
+		jq("#fruehbucher-eintritt").attr('checked', false);
+		jq("#regulaerer-eintritt").attr('checked', false);
+	});
+	
+});
