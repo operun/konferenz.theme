7505f362bf8558184f6c077bed0306adf08114f8
Steffen Lindner <mail@steffen-lindner.de>
2011-10-22 09:29:12 +0000

    - Student Anmeldung CSS Updates - Newsletter Portlet Styling
    git-svn-id: http://svn.operun.de/svn/konferenz/konferenz.theme/trunk@19 e724e7e9-2db1-4b50-8bfa-12963a624e1d

--- konferenz/theme/browser/javascripts/konferenz.js
+++ konferenz/theme/browser/javascripts/konferenz.js
@@ -5,16 +5,25 @@ jq(document).ready(function() {
 	jq("#fruehbucher-eintritt").click(function() {
 		jq("#regulaerer-eintritt").attr('checked', false);
 		jq("#student-hochschule-muenchen").attr('checked', false);
+		jq("#student-50-euro").attr('checked', false);
 	});
 
 	jq("#regulaerer-eintritt").click(function() {
 		jq("#fruehbucher-eintritt").attr('checked', false);
 		jq("#student-hochschule-muenchen").attr('checked', false);
+		jq("#student-50-euro").attr('checked', false);
 	});
 
 	jq("#student-hochschule-muenchen").click(function() {
 		jq("#fruehbucher-eintritt").attr('checked', false);
 		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-50-euro").attr('checked', false);
+	});
+	
+    jq("#student-50-euro").click(function() {
+		jq("#fruehbucher-eintritt").attr('checked', false);
+		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
 	});
 	
 });
