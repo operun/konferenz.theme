29f303f3a0c99818f8cf5ef72cad70723feadae8
Steffen Lindner <mail@steffen-lindner.de>
2012-01-30 18:34:50 +0000

    validation for registration
    git-svn-id: http://svn.operun.de/svn/konferenz/konferenz.theme/trunk@103 e724e7e9-2db1-4b50-8bfa-12963a624e1d

--- konferenz/theme/browser/javascripts/konferenz.js
+++ konferenz/theme/browser/javascripts/konferenz.js
@@ -37,24 +37,56 @@ jq(document).ready(function() {
 		jq("#regulaerer-eintritt").attr('checked', false);
 		jq("#student-hochschule-muenchen").attr('checked', false);
 		jq("#student-50-euro").attr('checked', false);
-	});
+	    jq("#mittwoch-22ster-februar").attr('checked', false);
+        jq("#donnerstag-23ster-februar").attr('checked', false);
+        jq("#freitag-24ster-februar").attr('checked', false);
+    });
 
 	jq("#regulaerer-eintritt").click(function() {
 		jq("#fruehbucher-eintritt").attr('checked', false);
 		jq("#student-hochschule-muenchen").attr('checked', false);
-		jq("#student-50-euro").attr('checked', false);
-	});
+		 jq("#student-50-euro").attr('checked', false);
+	    jq("#mittwoch-22ster-februar").attr('checked', false);
+        jq("#donnerstag-23ster-februar").attr('checked', false);
+        jq("#freitag-24ster-februar").attr('checked', false);
+    });
 
 	jq("#student-hochschule-muenchen").click(function() {
 		jq("#fruehbucher-eintritt").attr('checked', false);
 		jq("#regulaerer-eintritt").attr('checked', false);
 		jq("#student-50-euro").attr('checked', false);
-	});
+	    jq("#mittwoch-22ster-februar").attr('checked', false);
+        jq("#donnerstag-23ster-februar").attr('checked', false);
+        jq("#freitag-24ster-februar").attr('checked', false);
+    });
 	
     jq("#student-50-euro").click(function() {
 		jq("#fruehbucher-eintritt").attr('checked', false);
 		jq("#regulaerer-eintritt").attr('checked', false);
 		jq("#student-hochschule-muenchen").attr('checked', false);
+        jq("#mittwoch-22ster-februar").attr('checked', false);
+        jq("#donnerstag-23ster-februar").attr('checked', false);
+        jq("#freitag-24ster-februar").attr('checked', false);
 	});
+
+    jq("#mittwoch-22ster-februar").click(function() {
+		jq("#student-50-euro").attr('checked', false);
+		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
+	});
+
+    jq("#donnerstag-23ster-februar").click(function() {
+		jq("#student-50-euro").attr('checked', false);
+		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
+	});
+
+    jq("#freitag-24ster-februar").click(function() {
+		jq("#regulaerer-eintritt").attr('checked', false);
+		jq("#student-50-euro").attr('checked', false);
+		jq("#student-hochschule-muenchen").attr('checked', false);
+	});
+
+
 
 });
